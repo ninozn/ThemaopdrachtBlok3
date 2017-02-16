6eacbd4d773f822e883cc166d9db1d2a1d9d6e4a
ninozn <nino-zijderveld@hotmail.com>
2017-02-16 19:30:15 +0100

    Opdracht 1b - Functienamen en attribuutnamen aangepast aangezien deze niet duidelijk waren. Het was niet of moeilijk af te leiden waarvoor bepaalde attributen gebruikt werden. Ook de benoeming van bepaalde functies waren niet logisch.

--- src/javafx/WerkplaatsApp/domein/Monteur.java
+++ src/javafx/WerkplaatsApp/domein/Monteur.java
@@ -4,66 +4,69 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Monteur implements Serializable {
-	private String naam;
-	private double uurloon;
-	private int monteurNummer;
-	private ArrayList<Werkorder> deWerkOrders = new ArrayList<Werkorder>();
+    private String naam;
+    private double uurloon;
+    private int monteurNummer;
+    private ArrayList<Werkorder> alleWerkorders = new ArrayList<Werkorder>();
 
-	public Monteur(String nm, double ul, int mn) {
-		naam = nm;
-		uurloon = ul;
-		monteurNummer = mn;
-	}
-	
-	public boolean voegWerkOrderToe(Werkorder w){
-		boolean b = false;
-		if(!deWerkOrders.contains(w)){
-			deWerkOrders.add(w);
-			b = true;
-		}
-		return b;
-	}
-	public String getNaam() {
-		return naam;
-	}
+    public Monteur(String naam, double uurloon, int monteurNummer) {
+        this.naam = naam;
+        this.uurloon = uurloon;
+        this.monteurNummer = monteurNummer;
+    }
 
-	public void setNaam(String naam) {
-		this.naam = naam;
-	}
+    public boolean voegWerkOrderToe(Werkorder werkorder) {
+        boolean b = false;
+        if (!alleWerkorders.contains(werkorder)) {
+            alleWerkorders.add(werkorder);
+            b = true;
+        }
+        return b;
+    }
 
-	public double getUurloon() {
-		return uurloon;
-	}
+    public String getNaam() {
+        return naam;
+    }
 
-	public void setUurloon(double uurloon) {
-		this.uurloon = uurloon;
-	}
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
 
-	public int getMonteurNummer() {
-		return monteurNummer;
-	}
+    public double getUurloon() {
+        return uurloon;
+    }
 
-	public void setMonteurNummer(int monteurNummer) {
-		this.monteurNummer = monteurNummer;
-	}
-	public ArrayList<Werkorder> geefAlleWerkorder() { //methode om de hele arraylist van werkorders op te vragen die bij een monteur hoort
-		return deWerkOrders;
-	}
-	public boolean equals(Object obj) { //overridden versie van methode equals in klasse monteur, om zo een heel monteur object te kunnen vergelijken
-		boolean b;
-		if (obj instanceof Monteur) {
-			b = true;
-		}
-		else {
-			b = false;
-		}
-		b = b && (this.naam == ((Monteur)obj).naam);
-		b = b && (this.monteurNummer == ((Monteur)obj).monteurNummer);
-		b = b && (this.uurloon == ((Monteur)obj).uurloon);
-		return b;
-	}
-	public String toString() {
-		String s = naam + " " + monteurNummer;
-		return s;
-	}
+    public void setUurloon(double uurloon) {
+        this.uurloon = uurloon;
+    }
+
+    public int getMonteurNummer() {
+        return monteurNummer;
+    }
+
+    public void setMonteurNummer(int monteurNummer) {
+        this.monteurNummer = monteurNummer;
+    }
+
+    public ArrayList<Werkorder> geefAlleWerkorder() { //methode om de hele arraylist van werkorders op te vragen die bij een monteur hoort
+        return alleWerkorders;
+    }
+
+    public boolean equals(Object obj) { //overridden versie van methode equals in klasse monteur, om zo een heel monteur object te kunnen vergelijken
+        boolean b;
+        if (obj instanceof Monteur) {
+            b = true;
+        } else {
+            b = false;
+        }
+        b = b && (this.naam == ((Monteur) obj).naam);
+        b = b && (this.monteurNummer == ((Monteur) obj).monteurNummer);
+        b = b && (this.uurloon == ((Monteur) obj).uurloon);
+        return b;
+    }
+
+    public String toString() {
+        String s = naam + " " + monteurNummer;
+        return s;
+    }
 }
